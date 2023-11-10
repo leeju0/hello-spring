e2644c717987122da7fc2486ec0d33e68f818e46
leeju0 <92801522+leeju0@users.noreply.github.com>
2023-11-11 02:15:40 +0900

    MVC 디자인 패턴에 대해 공부했음

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -10,10 +11,13 @@ public class HelloController {
     @GetMapping("hello") //웹 에서 /hello url로 들어오면 아래 메소드호출
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
-                                       // key                 value
+        // key                 value
         return "hello"; //hello.html에게 가라
+    }
 
-
-
+    @GetMapping("hello-mvc") //이번엔 파라미터로 값을 받아보자. name값 받아오기
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
