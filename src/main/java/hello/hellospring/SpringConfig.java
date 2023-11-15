5d2683e3ea67f8ef5e2c5d42f4f8cd2bf8fd5b99
leeju0 <92801522+leeju0@users.noreply.github.com>
2023-11-16 02:15:59 +0900

    jdbc, h2 사용해보기, 리포지토리 바꿔보기

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,15 +1,24 @@
 package hello.hellospring;
 
-import hello.hellospring.domain.Member;
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration // -> 직접 스프링 컨테이너를 만드는것! @Configuration 붙어있으면 스프링 컨테이너임
 public class SpringConfig {
     //직접 빈 등록하고, 직접 의존성 주입해주는 방법입니다.
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -18,6 +27,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
